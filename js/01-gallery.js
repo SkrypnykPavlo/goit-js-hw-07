d27afe0fb27cc1c2b14ffe01d9ae8e3d75e20a7f
SkrypnykPavlo <pm.skrypnyk.p@gmail.com>
2023-01-05 01:32:06 +0200

    hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainerEl = document.querySelector('.gallery');
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML('beforeend', galleryMarkup);
 
-console.log(galleryItems);
+function makeGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+const onContainerClick = evt => {
+  evt.preventDefault();
+
+  const url = evt.target.dataset.source;
+  if (!url) return;
+
+  const instance = basicLightbox.create(`
+    <img src="${url}"width="800" height="600">`);
+
+  instance.show();
+};
+galleryContainerEl.addEventListener('click', onContainerClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainerEl = document.querySelector('.gallery');
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML('beforeend', galleryMarkup);
 
-console.log(galleryItems);
+function makeGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<ul class="gallery">
+      <li>
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"          
+          alt="${description}"
+        />
+      </a>
+      </li>
+     
+    </ul>`;
+    })
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionType: 'alt',
+});
